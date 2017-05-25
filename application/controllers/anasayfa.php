4ba66fd7705d21b974734a8f5c2161e5ef9e8003
Ender IMEN <enderimen@hotmail.com>
2017-05-25 22:35:22 +0300

    Get URL den parameter. (sample)

--- application/controllers/anasayfa.php
+++ application/controllers/anasayfa.php
@@ -17,6 +17,16 @@ class Anasayfa extends CI_Controller {
 			echo "Giriş Hatalı";
 		}
 	}
+	public function	parameters(){
+		$sayi=$this->uri->segment(1);//Kullanıcının girdiği parametreleri yakalamak için kullanılan yöntemdir.Url indisine göre değer alır
+		echo $sayi;
+	}
+
+	public function	parameters($p1,$p2){//Kullanıcının girdiği para. yakalamak için kullanılır.Url /..../..../ noktalı yerlere sırasıyla ne girilirse onu p1,p2 değişkenleri yakalar.
+		echo $sayi;
+		$sayi=$this->uri->segment(1);
+	}
+
 }
 
 ?>
--- application/views/form.php
+++ application/views/form.php
@@ -6,8 +6,8 @@
 </head>
 <body>
 	<form action="<?php echo base_url('anasayfa/giris'); ?>" method="POST"><!--base_url mize sub domain olarak anasayfa/giris kısmını gönderdik-->
-		<input type="text" name="kadi" placeholder="Kullanıcı Adı" required><br>
-		<input type="password" name="sifre" placeholder="Şifre" required><br>
+		<input type="text" name="kadi" placeholder="Kullanıcı Adı" required><br/>
+		<input type="password" name="sifre" placeholder="Şifre" required><br/>
 		<input type="submit">
 	</form>
 </body>
