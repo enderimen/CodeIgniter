e20b8ad2dd9326b5311b5eca4b57c14da818bfca
Ender IMEN <enderimen@hotmail.com>
2017-05-28 02:03:06 +0300

    where,or_where query examples.

--- application/controllers/dbislem.php
+++ application/controllers/dbislem.php
@@ -17,6 +17,36 @@ class dbislem extends CI_Controller
 		$viewData=array("rows"=>$rows);//tek satır okuduk ve değişkene aktardık
 
 		$this->load->view("dbislem",$viewData);
+
+	}
+
+
+	public function where()
+	{
+		$rows=$this//BU
+			->db//veritabanından
+			->where("personel_adi","Ender")->get("personel")//personel adı = Ender olan ..... where("personel_adi =!","Ender") Adı ender olmayanları getir 
+			->result();//kayıtları  getir.
+
+			print_r($rows);//yazdır.
+	}
+	public function or_where()
+	{
+		$rows=$this//BU
+			->db//veritabanından
+			->or_where("personel_adi","Ender")//Adı ender olan veya
+			->or_where("personel_adi","Ali")//adı Ali olan
+			->get("personel")//personel adı = Ender olan veya ali olan 
+			->result();//kayıtları  getir.
+
+
+
+			$viewData=array("rows"=>$rows);//tek satır okuduk ve değişkene aktardık
+
+			$this->load->view("dbislem",$viewData);
+
+
+			//print_r($rows);//yazdır.
 	}
 }
 
